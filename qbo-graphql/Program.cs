8b104549875c49dfeca2a054dbd255f821f24faa
Derek Batenhorst <derek.batenhorst@buildertrend.com>
2024-08-30 16:17:56 -0500

    initial commit

---
+++ qbo-graphql/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using qbo_graphql.StarWarsClient;
+using StrawberryShake;
+
+var serviceCollection = new ServiceCollection();
+
+serviceCollection
+    .AddStarWarsClient()
+    .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://swapi-graphql.netlify.app/.netlify/functions/index"));
+
+IServiceProvider services = serviceCollection.BuildServiceProvider();
+
+IStarWarsClient client = services.GetRequiredService<IStarWarsClient>();
+
+var result = await client.GetPlanets.ExecuteAsync();
+result.EnsureNoErrors();
+
+var planets = result.Data?.AllPlanets?.Planets ?? [];
+
+foreach (var planet in planets)
+{
+    Console.WriteLine(planet?.Name);
+}
+
+var films = await client.GetFilms.ExecuteAsync();
+films.EnsureNoErrors();
+
+var filmsData = films.Data?.AllFilms?.Films ?? [];
+foreach (var film in filmsData)
+{
+    Console.WriteLine(film?.Title);
+}
